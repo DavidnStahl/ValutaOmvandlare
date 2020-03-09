52ebc590e29f9214102242e8824a6d84b1b9fe64
47720480 <david.n.stahl@gmail.com>
2020-03-09 11:59:22 +0100

    update fixed bugg with cookieexpire 1hour

--- Modules/Repositories/CookiesRepository.js
+++ Modules/Repositories/CookiesRepository.js
@@ -1,12 +1,21 @@
 export default class CookieRepository {
     static  createListOfCurrancy(list){
         var today = new Date()
-        today.getTimezoneOffset()
-        today.setHours(today.getHours() + 1);
+        today.setHours(today.getHours(),today.getMinutes() + 60);
         document.cookie = `${list}; expires=${today}`
+        localStorage.setItem("currencyNameListExpireTime",`${today}`)
+
     }
     static  getListOfCurrancy(){
       let arr = document.cookie.split(",")
-      return arr
+      var expireTime = localStorage.getItem("currencyNameListExpireTime")
+      if(Date.parse(expireTime) < new Date()){
+
+        return "null"
+      }
+      else{
+        return arr
+      }
+      
     }
   }
